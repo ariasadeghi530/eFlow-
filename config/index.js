e04cacc6bad11bbb2dd828b119c98bcfcb366718
logbru <59076388+logbru@users.noreply.github.com>
2020-03-03 09:39:26 -0800

    added ip tracking

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 
 // sql://username:password@host:port/dbname
-const sequelize = new Sequelize('mysql://root:admin@localhost:3306/eflow')
+const sequelize = new Sequelize('mysql://root:password@localhost:3306/eflow')
 
 module.exports = sequelize
