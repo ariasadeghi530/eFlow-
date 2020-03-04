de11399d88311db3fad01d2c7c9a8286bab5dc4f
Carlo <csnazareno94@gmail.com>
2020-03-04 14:16:45 -0800

    ?

--- public/checklogin.js
+++ public/checklogin.js
@@ -2,12 +2,12 @@ axios.get(`/api/users/checklogin`)
   .then(({data}) => {
     if (data===''){
       // NOT LOGGED IN
-      if (window.location.pathname === '/login' || window.location.pathname === '/register/'){
+      if (window.location.pathname === '/login' || window.location.pathname === '/register'){
 
       }else{
         window.location.replace("/login")
       }
-    } else if (window.location.pathname === '/login' || window.location.pathname === '/register/'){
+    } else if (window.location.pathname === '/login' || window.location.pathname === '/register'){
       // logged in, check if trys to login/register
       window.location.replace("/")
     }
--- views/components/headerOne.jsx
+++ views/components/headerOne.jsx
@@ -3,11 +3,13 @@ const React = require('react')
 // Header with no navigation links - ONLY LOGO
 const HeaderOne = () => {
   return (
-    <nav className="uk-navbar uk-navbar-container uk-margin">
-      <div className="uk-navbar-center">
-        <a className="uk-navbar-item uk-logo" href="/">eFlow</a>
-      </div>
-    </nav>
+    <div uk-sticky="sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky">
+      <nav className="uk-navbar uk-navbar-container uk-margin nav-margin">
+        <div className="uk-navbar-center uk-padding-bottom">
+          <a className="uk-navbar-item uk-logo" href="/">eFlow</a>
+        </div>
+      </nav>
+    </div>
   )
 }
 
