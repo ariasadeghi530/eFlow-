30961275f1d196d6963ddffac12f538e69c1b6fa
Aria Sadeghi <ariasadeghi@gmail.com>
2020-03-04 10:07:23 -0800

    updated pagination styling

--- views/components/pagination.jsx
+++ views/components/pagination.jsx
@@ -6,13 +6,10 @@ const Pagination = () => {
     <ul className="uk-pagination" uk-margin="true">
       <li><a href="#"><span uk-pagination-previous="true"></span></a></li>
       <li><a href="#">1</a></li>
+      <li><a href="#">2</a></li>
+      <li><a href="#">3</a></li>
       <li className="uk-disabled"><span>...</span></li>
     
-      <li><a href="#">6</a></li>
-      <li className="uk-active"><span>7</span></li>
-      <li><a href="#">8</a></li>
-    
-      <li className="uk-disabled"><span>...</span></li>
       <li><a href="#">20</a></li>
       <li><a href="#"><span uk-pagination-next="true"></span></a></li>
     </ul>
