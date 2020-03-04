1f3345d5a9f3c0ffe7a12547fa43acb8d3612230
Aria Sadeghi <ariasadeghi@gmail.com>
2020-03-04 10:06:46 -0800

    added classes for users and items

--- models/Item.js
+++ models/Item.js
@@ -1,41 +1,41 @@
-// const { Model, STRING, INTEGER } = require('sequelize')
-// const sequelize = require('../config')
+const { Model, STRING, INTEGER } = require('sequelize')
+const sequelize = require('../config')
 
-// class Item extends Model { }
+class Item extends Model { }
 
-// Item.init({
-//   name: {
-//     type: STRING,
-//     allowNull: false
-//   },
-//   description: {
-//     type: STRING,
-//     allowNull: false
-//   },
-//   category: {
-//     type: STRING,
-//     allowNull: false
-//   },
-//   condition: {
-//     type: STRING,
-//     allowNull: false
-//   },
-//   price: {
-//     type: INTEGER,
-//     allowNull: false
-//   },
-//   sell_date: {
-//     type: STRING,
-//     allowNull: false
-//   },
-//   location: {
-//     type: STRING,
-//     allowNull: false
-//   },
-//   owner_id: {
-//     type: INTEGER,
-//     allowNull: false
-//   }
-// }, { sequelize, modelName: 'item' })
+Item.init({
+  name: {
+    type: STRING,
+    allowNull: false
+  },
+  description: {
+    type: STRING,
+    allowNull: false
+  },
+  category: {
+    type: STRING,
+    allowNull: false
+  },
+  condition: {
+    type: STRING,
+    allowNull: false
+  },
+  price: {
+    type: INTEGER,
+    allowNull: false
+  },
+  sell_date: {
+    type: STRING,
+    allowNull: false
+  },
+  location: {
+    type: STRING,
+    allowNull: false
+  },
+  owner_id: {
+    type: INTEGER,
+    allowNull: false
+  }
+}, { sequelize, modelName: 'item' })
 
-// module.exports = Item
+module.exports = Item
