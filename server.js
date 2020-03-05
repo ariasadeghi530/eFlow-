f9d3d94a618fa998f232cfaad3f3e45558d37cf0
logbru <59076388+logbru@users.noreply.github.com>
2020-03-05 12:01:42 -0800

    fixed login check

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const { join } = require('path')
 const app = express()
+const axios = require('axios')
 const sequelize = require('./config')
+const cookieSession = require('cookie-session')
 
 app.use(express.static(join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1', 'key2']
+}))
 
 app.set('views', join(__dirname, 'views'))
 app.set('view engine', 'jsx')
@@ -14,35 +21,63 @@ app.engine('jsx', require('express-react-views').createEngine())
 app.use(require('./routes'))
 
 app.get('/login', (req, res) => {
-  res.render('login')
+  if (req.session.isLoggedin === true) {
+    res.render('home')
+  } else {
+    res.render('login')
+  }
 })
 
 app.get('/register', (req, res) => {
-  res.render('register')
+  if (req.session.isLoggedin === true) {
+    res.render('home')
+  } else {
+    res.render('register')
+  }
 })
 
 app.get('/chat', (req, res) => {
+  if (req.session.isLoggedin === true) {
   res.render('userchat')
+}else {
+  res.render('login')
+}
 })
 
 app.get('/collections', (req, res) => {
-  res.render('collections')
+  if (req.session.isLoggedin === true) {
+    res.render('collections')
+  }else{
+    res.render('login')
+  }
 })
 
 app.get('/', (req, res) => {
-  res.render('home',
-  {
-    whatsHot: `What's Hot`,
-    whatsNew: `What's New`
-  })
+  if (req.session.isLoggedin===true){
+    res.render('home',
+      {
+        whatsHot: `What's Hot`,
+        whatsNew: `What's New`
+      })
+  }else{
+    res.render('login')
+  }
 })
 
 app.get('/products', (req, res) => {
+  if (req.session.isLoggedin === true) {
   res.render('products')
+  } else {
+    res.render('login')
+  }
 })
 
 app.get('/profile', (req, res) => {
+if (req.session.isLoggedin === true) {
   res.render('profile')
+}else {
+  res.render('login')
+}
 })
 
 sequelize.sync() //or .authenticate()
