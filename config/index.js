bafffd78b80f7aff205acfd0656a5be6b800fa7f
logbru <59076388+logbru@users.noreply.github.com>
2020-03-05 12:18:36 -0800

    changing db for heroku

--- config/index.js
+++ config/index.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize')
 
 // sql://username:password@host:port/dbname
-const sequelize = new Sequelize('mysql://root:password@localhost:3306/eflow')
+//  const sequelize = new Sequelize('mysql://root:password@localhost:3306/eflow')
+const sequelize = new Sequelize('mysql://nnkrvaq9e9rlcc55:jtkn6aeksjgx9671@izm96dhhnwr2ieg0.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/zmf7l8rel1272z43')
 
 module.exports = sequelize
