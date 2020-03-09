1528b22c14a7ca43271a0e46905ed23a60a190bb
Aria Sadeghi <ariasadeghi@gmail.com>
2020-03-09 13:13:54 -0700

    updated conditionals

--- views/products.jsx
+++ views/products.jsx
@@ -24,6 +24,8 @@ const Products = props => {
 
             <ul class="uk-slider-items uk-grid">
           {props.prod.uploads.map((item, index) => {
+            console.log(' ')
+            console.log(item)
            
               return (
                 <li class="uk-width-3-4">
@@ -32,6 +34,7 @@ const Products = props => {
                       </div>
                       </li>
                    )
+              
                    })}
         
             </ul>
