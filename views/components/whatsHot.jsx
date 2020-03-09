5d9a9babfe89246d0a15439210da07acfb81dee2
Aria Sadeghi <ariasadeghi@gmail.com>
2020-03-09 10:29:32 -0700

    updated variable name

--- views/components/whatsHot.jsx
+++ views/components/whatsHot.jsx
@@ -8,18 +8,18 @@ const WhatsHot = (props) => {
       <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1" uk-slider="true">
 
         <ul class="uk-slider-items uk-child-width-1-2 uk-child-width-1-3@m uk-grid">
-          {props.list.map(item => {
+          {props.list.map(hot => {
             return(
             <li className="home-sliders">
-              <a href={"/products/" + item.id}>
+              <a href={"/products/" + hot.id}>
                 <div class="uk-panel ef-card-home">
               <div className="uk-flex-inline">
                 <div class="uk-card uk-card-default uk-card-small ef-card-home">
                   <div class="uk-card-media-top uk-flex uk-flex-center">
-                        <img src={item.uploads.map(items => items.path)} className="ef-home-img" alt="" />
+                        <img src={hot.uploads.map(items => items.path)} className="ef-home-img" alt="" />
                   </div>
                   <div class="uk-card-body ef-card-body">
-                    <p class="uk-text-bold slider-product-title slider-margin-bottom-remove">{item.name}</p>
+                    <p class="uk-text-bold slider-product-title slider-margin-bottom-remove">{hot.name}</p>
 
                   </div>
                 </div>
