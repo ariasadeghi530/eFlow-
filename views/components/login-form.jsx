43d8d1caf4721e2b2f00c79da2d47ab5c10473bc
Aria Sadeghi <ariasadeghi@gmail.com>
2020-03-09 15:10:21 -0700

    updated account reset

--- views/components/login-form.jsx
+++ views/components/login-form.jsx
@@ -12,7 +12,7 @@ const LogIn = () => {
                 <input type="password" className="uk-input" id="loginPassword" placeholder="Password"></input>
             </div>
             <div className="uk-flex uk-flex-right">
-                <p><a href="api/forgetpasswordemail">Forgot Password?</a></p>
+                <p><a href="api/forgetpasswordemail">Forgot Account Info?</a></p>
             </div>
             <button type="button" id="login" className="uk-button uk-button-primary uk-width-1-1 uk-margin-small-bottom">Log In</button>
             <p className="font-weight-light">By logging in you agree to our <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Terms Of Service</a> and <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Privacy Policy</a>.</p>
