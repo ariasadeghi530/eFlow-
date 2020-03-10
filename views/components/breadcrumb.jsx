f8bb5ad8f5751660c794743f87617546425361a5
Carlo Nazareno <csnazareno94@gmail.com>
2020-03-09 18:35:50 -0700

    breadcrumb fix

--- views/components/breadcrumb.jsx
+++ views/components/breadcrumb.jsx
@@ -5,10 +5,9 @@ const Breadcrumb = (props) => {
   if (props.item) {
     return (
       <div className="uk-margin-top uk-margin-left ef-bc-margin">
-        <ul class="uk-breadcrumb uk-text-truncate">
-          <li><a href="/">Home</a></li>
-          <li><a href={"/collections/" + props.item.category} >{props.item.category}</a></li>
-            <li><a href={"/products/" + props.item.id} >{props.item.name}</a></li>
+        <ul class="uk-breadcrumb">
+          <li><a href={"/collections/" + props.item.category}>{props.item.category}</a></li>
+          <li><a href={"/products/" + props.item.id} className="uk-text-break">{props.item.name}</a></li>
         </ul>
       </div>
     )
