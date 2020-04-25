87c82f82efb283e7302e4d99ab5203895cef71e0
Aria Sadeghi <ariasadeghi@gmail.com>
2020-04-25 12:24:43 -0700

    new db

--- config/index.js
+++ config/index.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize')
 // const sequelize = new Sequelize('mysql://root:admin@localhost:3306/eflow')
 const Op = Sequelize.Op
 
-const sequelize = new Sequelize('mysql://c209p3iee3snfxhw:k1b6iv4if637owrc@nba02whlntki5w2p.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/q3p72mds4851vgnu')
+const sequelize = new Sequelize('mysql://t1onobqe6mrucc93:f5mpr3ph87m3nu8h@bmsyhziszmhf61g1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/oe38tvrn3wsb85iv')
 // const sequelize = new Sequelize('mysql://root:spikey11@localhost:3306/eFLow')
 
 module.exports = sequelize
